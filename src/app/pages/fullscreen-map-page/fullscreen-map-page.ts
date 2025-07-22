593840d437a8e405773bf8a758cc0190e0c4f3cd
juan manuel <jmgv87191@gmail.com>
2025-07-22 09:00:03 -0700

    s

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.ts
@@ -1,13 +1,14 @@
 import { AfterViewInit, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import mapboxgl from 'mapbox-gl'; 
 import { environment } from '../../../environments/environment';
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe, JsonPipe } from '@angular/common';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = environment.mapboxkey
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [ DecimalPipe ],
+  imports: [ DecimalPipe, JsonPipe ],
   templateUrl: './fullscreen-map-page.html',
   styleUrl: './fullscreen-map-page.css'
 })
@@ -15,6 +16,11 @@ export class FullscreenMapPage implements AfterViewInit {
 
   divElement = viewChild<ElementRef>('map')
   map = signal<mapboxgl.Map | null>(null) 
+
+  coordinates = signal({
+    lng : -74.5,
+    lat: 40
+  })
 
   zoomEffect = effect(()=>{
     if (!this.map())return;
@@ -34,12 +40,12 @@ export class FullscreenMapPage implements AfterViewInit {
 
 
     const element = this.divElement()!.nativeElement
-    console.log('element')
+    const { lat, lng } = this.coordinates();
 
     const map = new mapboxgl.Map({
-    	container: element, // container ID
-    	style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    	center: [-74.5, 40], // starting position [lng, lat]
+      container: element, 
+      style: 'mapbox://styles/mapbox/streets-v12', 
+      center: [lng, lat], 
       zoom: this.zoom(), 
     });
 
@@ -52,7 +58,22 @@ export class FullscreenMapPage implements AfterViewInit {
       const newZoom = event.target.getZoom()
       this.zoom.set(newZoom)
     })
+    
+    map.on( 'moveend',()=>{
+      const center = map.getCenter()
+      this.coordinates.set(center)
+    } )
+
+/*     map.on( 'load',()=>{
+      console.log('el mapa uya esta caradi')
+    })
+     */
+
+    
+    map.addControl( new mapboxgl.FullscreenControl() )
+    map.addControl( new mapboxgl.NavigationControl() )
+    map.addControl( new mapboxgl.ScaleControl() )
+    
     this.map.set( map )
   }
-
 }
--- src/app/pages/markers-page/markers-page.ts
+++ src/app/pages/markers-page/markers-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
+import mapboxgl from 'mapbox-gl'; 
+import { environment } from '../../../environments/environment';
+mapboxgl.accessToken = environment.mapboxkey
 
 @Component({
   selector: 'app-markers-page',
@@ -6,6 +9,65 @@ import { Component } from '@angular/core';
   templateUrl: './markers-page.html',
   styleUrl: './markers-page.css'
 })
-export class MarkersPage {
+export class MarkersPage implements AfterViewInit {
+
+  divElement = viewChild<ElementRef>('map')
+  map = signal<mapboxgl.Map | null>(null) 
+
+
+
+
+
+
+  async ngAfterViewInit() {
+    if( !this.divElement()?.nativeElement ) return;
+
+    await new Promise((resolve)=> setTimeout(resolve, 1000))
+
+    const element = this.divElement()!.nativeElement
+
+    const map = new mapboxgl.Map({
+      container: element, 
+      style: 'mapbox://styles/mapbox/streets-v12', 
+      center: [-122.409850, 37.793085], 
+      zoom: 14, 
+    });
+
+/*     const marker = new mapboxgl.Marker({
+      draggable:false, color:'black'
+    }).setLngLat([-122.409850, 37.793085]).addTo(map)
+     */
+
+
+
+
+
+
+
+    this.mapListeners(map);
+  }
+  
+  mapListeners(map: mapboxgl.Map){
+
+    map.on('click',(event)=> this.mapClick(event))
+
+    this.map.set(map)
+  }
+  
+  mapClick( event: mapboxgl.MapMouseEvent ){
+
+    const coords = event.lngLat
+    const map =this.map()!
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+    (Math.random() *16 |  0).toString(16)
+    )
+
+      const marker = new mapboxgl.Marker({
+      draggable:false, color:color
+    }).setLngLat( coords ).addTo( map )
+    
+
+  }
+
 
 }
